640a24387f558a8a231c4074521690d6989ca593
Giljape <dyghks123@gmail.com>
2020-11-01 16:47:46 +0900

    Updates

---
+++ precache-manifest.2046500a525545ea3d383cda44266455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5146cf12fe2eca36db8b1419bd0856d6",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4ff72efdefbb1aecd7e9",
+    "url": "/movie_app_2020/static/css/main.3899e469.chunk.css"
+  },
+  {
+    "revision": "f82c9e08b7f14abd950d",
+    "url": "/movie_app_2020/static/js/2.ad0a88ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.ad0a88ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff72efdefbb1aecd7e9",
+    "url": "/movie_app_2020/static/js/main.1273a05b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
